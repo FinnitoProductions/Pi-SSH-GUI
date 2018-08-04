3f08176a39467c2feb4bfa7b2442de1c02cb1d95
FinnitoProductions <finn@teachmy.com>
2018-08-04 11:49:53 +0200

    Configured project to work with VS Code

--- src/util/SSHUtil.java
+++ src/util/SSHUtil.java
@@ -9,7 +9,7 @@ import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpException;
+import com.jcraft.jsch.SftpException; 
 
 import window.AppWindow;
 
