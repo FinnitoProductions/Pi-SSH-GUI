6ca760c8b3c298fe07ba23c7c0eea48f3ab14cb5
FinnitoProductions <finn@teachmy.com>
2018-08-06 16:09:20 +0200

    Modified SSH utility to reset piped input/output with each run

--- src/util/SSHUtil.java
+++ src/util/SSHUtil.java
@@ -3,73 +3,68 @@ package util;
 import java.awt.Color;
 import java.awt.Font;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpException; 
+import com.jcraft.jsch.SftpException;
 
 import window.AppWindow;
 
 /**
  * Contains multiple methods for interfacing with an SSH device.
+ * 
  * @author Finn Frankis
  * @version Aug 3, 2018
  */
-public class SSHUtil
-{
+public class SSHUtil {
 
     /**
      * Forms the initial SSH connection with the Pi.
+     * 
+     * @param window the window class which will be used to connect
      */
-    public static void connectSSH(AppWindow window) throws Exception
-    {
-        try
-        {
+    public static void connectSSH (AppWindow window) {
+        try {
             JSch jsch = new JSch();
-            
+
             window.setSession(jsch.getSession(Constants.PI_USER, Constants.PI_IP, Constants.PI_PORT));
             window.getSession().setPassword(Constants.PI_PASSWORD);
             window.getSession().setConfig("StrictHostKeyChecking", "no");
             window.getLblSshConnected().setText("Connecting...");
             window.getLblSshConnected().setForeground(Color.BLUE);
             window.getSession().connect();
-            
-            if (window.getSession().isConnected())
-            {
+
+            if (window.getSession().isConnected()) {
                 window.getLblSshConnected().setText("Pi Connected");
                 window.getLblSshConnected().setForeground(new Color(105, 196, 80));
                 Font f = window.getLblSshConnected().getFont();
                 window.getLblSshConnected().setFont(f.deriveFont(f.getStyle() | Font.BOLD));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             window.getLblSshConnected().setText("Pi Not Connected");
             window.getLblSshConnected().setForeground(Color.RED);
             window.getBtnDeploy().setEnabled(false);
             window.getBtnRun().setEnabled(false);
         }
     }
-    
+
     /**
      * Transfers the actively selected file to the Pi via SFTP.
      */
-    public static void transferFile (AppWindow window, File f) 
-    {
-        if (window.getSession().isConnected() && f != null)
-        {
-            try
-            {
+    public static void transferFile (AppWindow window, File f) {
+        if (window.getSession().isConnected() && f != null) {
+            try {
                 ChannelSftp sftpChannel = (ChannelSftp) window.getSession().openChannel("sftp");
                 sftpChannel.connect();
                 sftpChannel.put(f.getAbsolutePath(), f.getName());
+                System.out.println(f.getName());
                 sftpChannel.disconnect();
-            }
-            catch (JSchException | SftpException e)
-            {
+            } catch (JSchException | SftpException e) {
                 window.getErrorLabel().setText("ERROR: File could not be transferred.");
             }
         }
@@ -78,25 +73,28 @@ public class SSHUtil
     /**
      * Runs the deployed code on the Pi.
      */
-    public static void runCode (AppWindow window, File f)
-    {
-        if (window.getSession().isConnected() && f != null)
-        {
-            try
-            {
+    public static void runCode (AppWindow window, File f) {
+        if (window.getSession().isConnected() && f != null) {
+            try {
                 window.setClExec(window.getSession().openChannel("exec"));
-                window.getClExec().setOutputStream(System.out);
-                window.getClExec().setInputStream(window.getInputOutput().getInputStream());
-    
-                ((ChannelExec)window.getClExec()).setCommand("sudo java -jar " + f.getName());
+                
+                if (window.getSystemOut() != null)
+                    window.getSystemOut().stop();
+                window.setSystemOut(new PipedInputOutputWrapper());
+                window.getClExec().setOutputStream(window.getOutReader().getOutputStream());
+                
+                if (window.getSSHCommandValue() != null)
+                    window.getSSHCommandValue().stop();
+                window.setSSHCommandValue(new PipedInputOutputWrapper());
+                window.getClExec().setInputStream(window.getSSHCommandValue().getInputStream());
+
+                ((ChannelExec) window.getClExec()).setCommand("sudo java -jar " + f.getName());
                 window.getClExec().connect();
-            }
-            catch (JSchException e)
-            {
+            } catch (JSchException e) {
                 window.getErrorLabel().setText("ERROR: Code could not be run.");
             }
-            
+
         }
     }
-    
+
 }
