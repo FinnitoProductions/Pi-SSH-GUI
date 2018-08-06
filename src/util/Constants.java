15b35ab4995a34f23c9a980fb72ebc1c370bfec3
FinnitoProductions <finn@teachmy.com>
2018-08-06 16:11:41 +0200

    Added force stop constant for program in keyboard

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,4 +1,5 @@
 package util;
+
 import java.io.File;
 
 /**
@@ -6,8 +7,7 @@ import java.io.File;
  * @author Finn Frankis
  * @version Aug 3, 2018
  */
-public class Constants 
-{
+public class Constants {
     public static String PI_USER = "pi";
     public static String PI_PASSWORD = "team1072";
     public static String PI_IP = "192.168.178.63";
@@ -17,15 +17,16 @@ public class Constants
     public static final int FRAME_LOCATION_Y = 100;
     public static final int FRAME_SIZE_X = 766;
     public static final int FRAME_SIZE_Y = 600;
-    
+
     public static final String INT_K_BIND_PATH = "/bindings.json";
     public static final String USER_HOME = System.getProperty("user.home") + File.separator;
     public static final String EXT_DIR_NAME = "pi-ssh-app";
     public static final String EXT_DIR_PATH = USER_HOME + EXT_DIR_NAME;
     public static final String EXT_K_BIND_PATH = EXT_DIR_PATH + File.separator + "bindings.json";
-    
+
     public static final String K_BIND_UP_KEY = "up";
     public static final String K_BIND_DOWN_KEY = "down";
     public static final String K_BIND_LEFT_KEY = "left";
     public static final String K_BIND_RIGHT_KEY = "right";
+    public static final String K_BIND_STOP_CHAR = "^C";
 }
