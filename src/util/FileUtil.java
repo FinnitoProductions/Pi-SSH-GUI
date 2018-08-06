c6b2d63a1fdd3e92a9d625c0fd2b99d4aa188b5a
FinnitoProductions <finn@teachmy.com>
2018-08-06 16:11:04 +0200

    Documented and formatted FileUtil

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -9,46 +9,49 @@ import java.io.InputStreamReader;
 import window.AppWindow;
 
 /**
- * Represents multiple methods for handling both internal and external files.
+ * Contains multiple methods for handling both internal and external files.
  * @author Finn Frankis
  * @version Aug 3, 2018
  */
-public class FileUtil
-{
+public class FileUtil {
     /**
-     * Converts the contents of a given external file (stored in a directory not linked to the project)
-     * into a String format.
+     * Converts the contents of a given external file (stored in a directory not
+     * linked to the project) into a String format.
      * @param fileName the name of the file to be converted
      * @return the String format of the given file
      * @throws Exception if the file cannot be found or read from
      */
-    public static String getStringFromExternalFile (String fileName) throws Exception
-    {
+    public static String getStringFromExternalFile (String fileName)
+            throws Exception {
         String fileContents = "";
-    
+
         BufferedReader br = new BufferedReader(new FileReader(fileName));
-    
+
         while (br.ready())
-            fileContents += br.readLine() + System.getProperty("line.separator");
+            fileContents += br.readLine()
+                    + System.getProperty("line.separator");
         System.out.println(fileContents);
         return fileContents;
     }
 
     /**
-     * Converts the contents of a given local file (stored in a project directory past src).
-     * into a String format.
+     * Converts the contents of a given local file (stored in a project directory
+     * past src). into a String format.
      * @param fileName the name of the file to be converted
      * @return the String format of the given file
      * @throws Exception if the file cannot be found or read from
      */
-    public static String getStringFromLocalFile (String fileName) throws Exception
-    {
+    public static String getStringFromLocalFile (String fileName)
+            throws Exception {
         String fileContents = "";
-    
-        BufferedReader br = new BufferedReader(new InputStreamReader(AppWindow.getInstance().getClass().getResourceAsStream(Constants.INT_K_BIND_PATH)));
-    
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(AppWindow.getInstance().getClass()
+                        .getResourceAsStream(Constants.INT_K_BIND_PATH)));
+
         while (br.ready())
-            fileContents += br.readLine() + System.getProperty("line.separator");
+            fileContents += br.readLine()
+                    + System.getProperty("line.separator");
         System.out.println(fileContents);
         return fileContents;
     }
@@ -59,9 +62,9 @@ public class FileUtil
      * @param newContents the String containing the new file contents
      * @throws Exception if the file cannot be find or written to
      */
-    public static void writeStringToFile (String fileName, String newContents) throws Exception
-    {
-        BufferedWriter br = new BufferedWriter (new FileWriter(fileName));
+    public static void writeStringToFile (String fileName, String newContents)
+            throws Exception {
+        BufferedWriter br = new BufferedWriter(new FileWriter(fileName));
         br.write(newContents);
         br.close();
     }
